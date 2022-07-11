06f66550fe8009c8ddeb39450f703aacdbca6b74
Benjamin <benjapabloramos2002@gmail.com>
2022-07-11 16:27:37 -0300

    se agrego un buscador funcional y un me gusta

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//style in scripts
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
@@ -12,8 +13,32 @@ navbar.classList.remove('active');}
 let busqueda = document.querySelector('#search');
 let buscador_grande = document.querySelector('.search-from');
 busqueda.onclick =() =>{
-	buscador_grande.classList.toggle('gato');}
+	buscador_grande.classList.toggle('search-top');}
 
 let close = document.querySelector('#close');
 close.onclick =() =>{
-buscador_grande.classList.remove('gato')}
+buscador_grande.classList.remove('search-top')}
+let close_two = document.querySelector("#logo_buscar")
+close_two.onclick=() =>{
+buscador_grande.classList.toggle('search-top');}
+
+
+//close style in scripts 
+
+//open search
+let search = document.querySelector('#search-box');
+
+search.addEventListener('keyup',pruebas =()=>{
+let recetas = document.querySelectorAll(".receta")
+// search quit focust text in the search
+recetas.forEach(cocina=>{
+let texto_cocina = cocina.innerText.toLowerCase()
+nombre =search.value.toLowerCase();
+if (texto_cocina.includes(nombre) ){
+		cocina.classList.remove("filtro");
+} else {
+		cocina.classList.add("filtro");
+}
+})//recetas.foreach
+}//addEventListener
+)
