83a29e650f40a2f874fdad1b1b444f459e8956dc
Benjamin <benjapabloramos2002@gmail.com>
2022-08-03 20:26:19 -0300

    se agragaron las paginas y clases  para pollo,carne,postres,pastas tambine se creo y se esta modificando la pagina admin

--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,75 @@
-//style in scripts
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+// style in scripts
+const menu = document.querySelector('#menu-bars');
+const navbar = document.querySelector('.navbar');
 
-menu.onclick =() =>{
-menu.classList.toggle('fa-times');
-navbar.classList.toggle('active');}
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+};
 
-window.onscroll =() =>{
-menu.classList.remove("fa-times");
-navbar.classList.remove("active");}
-
-let busqueda = document.querySelector('#search');
-let buscador_grande = document.querySelector('.search-from');
-busqueda.onclick =() =>{
-buscador_grande.classList.toggle('search-top');
-search.addEventListener("keyup",pruebas)
-let recetas = document.querySelectorAll(".receta")
-recetas.forEach(cocina => {
-		cocina.classList.remove("filtro");
-})
-}
+const busqueda = document.querySelector('#search');
+const BuscadorGrande = document.querySelector('.search-from');
+busqueda.onclick = () => {
+  BuscadorGrande.classList.toggle('search-top');
+  search.addEventListener('keyup', pruebas);
+  const recetas = document.querySelectorAll('.receta');
+  recetas.forEach((cocina) => {
+    cocina.classList.remove('filtro');
+  });
+};
 
 search = document.querySelector('#search-box');
 
+const close = document.querySelector('#close');
+close.onclick = () => {
+  BuscadorGrande.classList.remove('search-top');
+  search.removeEventListener('keyup', pruebas);
+  search.value = '';
+};
+const CloseTow = document.querySelector('#logo_buscar');
+CloseTow.onclick = () => {
+  BuscadorGrande.classList.toggle('search-top');
+  search.removeEventListener('keyup', pruebas);
+};
 
-let close = document.querySelector('#close');
-close.onclick =() =>{
-buscador_grande.classList.remove('search-top')
-search.removeEventListener("keyup",pruebas)
-search.value = ''
+// close style in scripts
+
+// open search
+
+search.addEventListener(
+  'keyup',
+  (pruebas = () => {
+    const recetas = document.querySelectorAll('.receta');
+    // search quit focust text in the search
+    recetas.forEach((cocina) => {
+      const TextoCocina = cocina.innerText.toLowerCase();
+      nombre = search.value.toLowerCase().trim();
+      if (TextoCocina.includes(nombre)) {
+        cocina.classList.remove('filtro');
+      } else {
+        cocina.classList.add('filtro');
+      }
+    }); // recetas.foreach
+  }) // addEventListener
+);
+// cambia el color de los corazones
+const corazones = document.querySelectorAll('.recetas .fa-heart');
+
+for (const corazo of corazones) {
+  corazo.addEventListener('click', function() {
+    if (corazo.classList.contains('fa-regular')) {
+      corazo.classList.replace('fa-regular', 'fa-solid');
+      corazo.style.color = '#cf624c';
+    } else {
+      corazo.classList.replace('fa-solid', 'fa-regular');
+      corazo.style.color = 'black';
+    }
+  });
 }
-let close_two = document.querySelector("#logo_buscar")
-close_two.onclick=() =>{
-buscador_grande.classList.toggle('search-top');
-search.removeEventListener("keyup",pruebas)
-}
 
-//close style in scripts 
-
-//open search
-
-search.addEventListener('keyup',pruebas =()=>{
-let recetas = document.querySelectorAll(".receta")
-// search quit focust text in the search
-recetas.forEach(cocina=>{
-let texto_cocina = cocina.innerText.toLowerCase()
-nombre = search.value.toLowerCase().trim();
-if (texto_cocina.includes(nombre) ){
-		cocina.classList.remove("filtro");
-} else {
-		cocina.classList.add("filtro");
-}
-})//recetas.foreach
-}//addEventListener
-)
-
-let corazones = document.querySelectorAll(".recetas .fa-heart")
-
-for (const corazo of corazones){
-corazo.addEventListener("click",function(){
-if (corazo.classList.contains("fa-regular")) {
-corazo.classList.replace("fa-regular", "fa-solid")
-corazo.style.color = "#cf624c";}
-
-else{
-corazo.classList.replace("fa-solid", "fa-regular")
-corazo.style.color = "black";}})}
-
+const receta = document.querySelector('.receta');
+receta.onclick = function() {};
